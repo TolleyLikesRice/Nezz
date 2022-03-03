17095e00a203db3b354a3bb5d137f6a2f2087e48
TolleyLikesRice <tolleybjcoding@gmail.com>
2022-03-03 20:52:30 +0000

    Random number command

---
+++ commands/random.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('random')
+        .setDescription('Generate a random number')
+        .addIntegerOption(option =>
+            option.setName('min')
+                .setDescription('Minimum of the random number')
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName('max')
+                .setDescription('Maximum of the random number')
+                .setRequired(true)),
+    async execute(interaction) {
+        const min = interaction.options.getInteger('min')
+        const max = interaction.options.getInteger('max')
+        interaction.reply(`Your random number between ${min} and ${max} is: ${Math.floor(Math.random() * (max - min) + min).toString()}`)
+    },
+};
