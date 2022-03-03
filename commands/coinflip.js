9b473b10ad619d8d627400dd906f6bda5cd0268f
TolleyLikesRice <tolleybjcoding@gmail.com>
2022-03-03 20:59:46 +0000

    coinflip

---
+++ commands/coinflip.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('coinflip')
+        .setDescription('Flip a coin'),
+    async execute(interaction) {
+        let coin = Math.round(Math.random());
+        if (coin == 0) coin = "Tails";
+        else if (coin == 1) coin = "Heads";
+        else coin = "Error"
+        interaction.reply(coin)
+    },
+};
